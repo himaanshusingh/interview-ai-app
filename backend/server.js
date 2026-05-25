fb0e51eee5f5aaa2e454549b2fb4213d525d1601
Himanshu Singh <himanshusingh1k@gmail.com>
2026-05-25 22:11:32 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
+const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 
 connectToDB();
+
+if (process.env.NODE_ENV === "production") {
+  // const __filename = fileURLToPath(import.meta.url);
+  // const __dirname = path.dirname(__filename);
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("API is running..."));
+}
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
