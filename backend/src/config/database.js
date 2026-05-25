6ca97c939f8ac027557295e2ab62b01eb7811cf9
Himanshu Singh <himanshusingh1k@gmail.com>
2026-05-25 22:02:29 +0530

    commit

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectToDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}/interview-ai-app`);
 
     console.log("Connected to Database");
   } catch (err) {
